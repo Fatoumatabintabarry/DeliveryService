9159e01525037174700a5504181a822a0836e015
Fatoumatabintabarry <97992954+Fatoumatabintabarry@users.noreply.github.com>
2023-11-30 13:25:08 -0500

    front end modification of request a delivery

--- client/src/Pages/RequestDelivery.jsx
+++ client/src/Pages/RequestDelivery.jsx
@@ -89,8 +89,8 @@ export default class RequestDelivery extends React.Component {
 
   render() {
     return (
-      <div className="flex h-screen ">
-        <div className="m-auto">
+      <div className="flex h-screen mb-36 ">
+        <div className="m-auto w-full max-w-4xl">
           {/* Sender Information */}
           <div className="mt-5 bg-white rounded-lg shadow">
             <div className="flex">
@@ -138,17 +138,7 @@ export default class RequestDelivery extends React.Component {
                 placeholder="Address"
                 className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200 focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
               />
-              <div class="flex">
-                  <div class="flex-grow w-1/4 pr-2">
-                 <input  placeholder="Postal code " class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"/>
-                 </div>
-                 <div class="flex-grow w-1/4 pr-2">
-                  <input placeholder="City" class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"/>
-                  </div>
-                  <div class="flex-grow w-1/4 pr-2">
-                  <input placeholder="country" class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"/>
-                  </div>
-                </div>
+            
             </div>
 
             
@@ -195,20 +185,6 @@ export default class RequestDelivery extends React.Component {
                 className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200 focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
               />
 
-                 <div class="flex">
-                  <div class="flex-grow w-1/4 pr-2">
-                 <input  placeholder="Postal code " class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"/>
-                 </div>
-                 <div class="flex-grow w-1/4 pr-2">
-                  <input placeholder="City" class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"/>
-                  </div>
-                  <div class="flex-grow w-1/4 pr-2">
-                  <input placeholder="country" class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"/>
-                  </div>
-                </div>
-
-
-                
 
             </div>
 
@@ -216,7 +192,7 @@ export default class RequestDelivery extends React.Component {
               <div class="flex-1 py-5 pl-5 oPostal codeverflow-hidden">
              
                 <h1 class="inline text-2xl font-semibold leading-none">
-                 Total Cost
+                  Cost
                 </h1>
               </div>
             </div>
@@ -236,54 +212,42 @@ export default class RequestDelivery extends React.Component {
             
 
             <hr className="mt-4" />
-            <div className="flex flex-row-reverse p-3">
-              {/* Buttons and SVGs */}
-              <button
-                onClick={() => this.saveInfo()}
-                type="button"
-                class="flex items-center px-5 py-2.5 font-medium tracking-wide text-white capitalize   bg-black rounded-md hover:bg-gray-800  focus:outline-none focus:bg-gray-900  transition duration-300 transform active:scale-95 ease-in-out"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 0 24 24"
-                  width="24px"
-                  fill="#FFFFFF"
-                >
-                  <path d="M0 0h24v24H0V0z" fill="none"></path>
-                  <path
-                    d="M5 5v14h14V7.83L16.17 5H5zm7 13c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-8H6V6h9v4z"
-                    opacity=".3"
-                  ></path>
-                  <path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm2 16H5V5h11.17L19 7.83V19zm-7-7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zM6 6h9v4H6z"></path>
-                </svg>
-                <span class="pl-2 mx-1">Save</span>
-              </button>
-              <div class="flex-initial">
-                <button
-                  type="button"
-                  class="flex items-center px-5 py-2.5 font-medium tracking-wide text-black capitalize rounded-md  hover:bg-red-200 hover:fill-current hover:text-red-600  focus:outline-none  transition duration-300 transform active:scale-95 ease-in-out"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24px"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                  >
-                    <path d="M0 0h24v24H0V0z" fill="none"></path>
-                    <path d="M8 9h8v10H8z" opacity=".3"></path>
-                    <path d="M15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z"></path>
-                  </svg>
-                  <span class="pl-2 mx-1">Delete</span>
-                </button>
+          
+          
+              
+              
+          {/* Pre-filled Addresses */}
+          <div className="mt-5 bg-white shadow cursor-pointer rounded-xl">
+
+          <div class=" px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6 ">
+            <h3 class="text-xl dark:text-white font-semibold leading-5 text-gray-800">Summary</h3>
+            <div class="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
+              <div class="flex justify-between w-full">
+                <p class="text-base dark:text-white leading-4 text-gray-800">Subtotal</p>
+                <p class="text-base dark:text-gray-300 leading-4 text-gray-600">$56.00</p>
               </div>
-              {/* Button */}
-              <button
+              
+              <div class="flex justify-between items-center w-full">
+                <p class="text-base dark:text-white leading-4 text-gray-800">taxPrice</p>
+                <p class="text-base dark:text-gray-300 leading-4 text-gray-600">$8.00</p>
+              </div>
+            </div>
+            <div class="flex justify-between items-center w-full">
+              <p class="text-base dark:text-white font-semibold leading-4 text-gray-800">Total</p>
+              <p class="text-base dark:text-gray-300 font-semibold leading-4 text-gray-600">$36.00</p>
+            </div>
+
+            
+       
+              
+            </div>
+              {/* Button request delivery*/}
+           <button
                 type="button"
                 className="relative w-full flex 
-        justify-center items-center px-5 py-2.5 font-medium tracking-wide text-white 
-        capitalize bg-[#5F54E3] rounded-md hover:bg-gray-900 focus:outline-none transition 
-        duration-300 transform active:scale-95 ease-in-out"
+                  justify-center items-center px-5 py-2.5 font-medium tracking-wide text-white 
+                  capitalize bg-[#5F54E3] rounded-md hover:bg-gray-900 focus:outline-none transition 
+                  duration-300 transform active:scale-95 ease-in-out"
               >
                 {/* SVG */}
                 <svg
@@ -308,54 +272,14 @@ export default class RequestDelivery extends React.Component {
                 </button>
               </button>
             </div>
+            
           </div>
 
-          <button onClick={() => this.checkInfos()}>check infos</button>
+         
 
-          {/* Pre-filled Addresses */}
-          <div className="mt-5 bg-white shadow cursor-pointer rounded-xl">
-            <div className="flex">
-              {/* Address details */}
-              <div class="flex-1 py-5 pl-5 overflow-hidden">
-                <ul>
-                  <li class="text-xs text-gray-600 uppercase ">Receiver</li>
-                  <li>{this.state.nameReceiver}</li>
-                  <li>Musterstrasse 1</li>
-                  <li>{this.state.addressReceiver}</li>
-                </ul>
-              </div>
-              <div class="flex-1 py-5 pl-1 overflow-hidden">
-                <ul>
-                  <li class="text-xs text-gray-600 uppercase">Ship from</li>
-                  <li>{this.state.nameSender}</li>
-                  <li>Rickrolled 11</li>
-                  <li>{this.state.addressSender}</li>
-                </ul>
-              </div>
-              <div class="flex-none pt-2.5 pr-2.5 pl-1">
-                <button
-                  type="button"
-                  class="px-2 py-2 font-medium tracking-wide text-black capitalize transition duration-300 ease-in-out transform rounded-xl hover:bg-gray-300 focus:outline-none active:scale-95"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24px"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                    fill="#000000"
-                  >
-                    <path d="M0 0h24v24H0V0z" fill="none"></path>
-                    <path
-                      d="M5 18.08V19h.92l9.06-9.06-.92-.92z"
-                      opacity=".3"
-                    ></path>
-                    <path d="M20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29s-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83zM3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM5.92 19H5v-.92l9.06-9.06.92.92L5.92 19z"></path>
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
+
+        
       </div>
     );
   }
